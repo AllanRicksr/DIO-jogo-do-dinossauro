2c571a5cb5d0932cb1573a8844f46b6729c72331
AllanRicksr <allanrickrodrigues@hotmail.com>
2022-05-05 15:39:49 -0400

    inicando codigo do jogo do dinossauro

---
+++ script.js
@@ -0,0 +1,8 @@
+const dino = document.querySelector('.dino');
+
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+        console.log('pressionou espaço');
+    }
+}
+document.addEventListener('keyup', handleKeyUp)
